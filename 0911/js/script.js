326873016f3117dbf854b2af9cb44506a0ffe69f
Emanuellyrio <lethicia.alves.estudante@eteot.rio.br>
2021-11-16 17:59:31 -0300

    att 16.11
    terminar até a próxima semana

---
+++ 0911/js/script.js
@@ -0,0 +1,16 @@
+
+function onScroll(){
+    if($(window).scrollTop()<250){
+        $('.scroll').css('display','none');
+     }else{
+         $('.scroll').css('display','block');
+     }
+}
+window.addEventListener('scroll',onScroll,false);
+
+jQuery(document).ready(function($){
+    $('.link').click(function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top},800)
+    });
+});
